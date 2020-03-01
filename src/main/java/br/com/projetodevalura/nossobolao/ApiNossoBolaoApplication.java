4025134ee96776c59980562c3b87d3c2f9196f36
Mauricio <maugravena@gmail.com>
2020-03-01 13:07:21 -0300

    Primeiro commit

---
+++ src/main/java/br/com/projetodevalura/nossobolao/ApiNossoBolaoApplication.java
@@ -0,0 +1,12 @@
+package br.com.projetodevalura.nossobolao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNossoBolaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNossoBolaoApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/projetodevalura/nossobolao/ApiNossoBolaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetodevalura.nossobolao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNossoBolaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
